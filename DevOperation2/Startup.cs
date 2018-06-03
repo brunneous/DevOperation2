bd49ec6003853c6303f9b948523371bb23c94491
Noah Brunn <noah@brunn.ca>
2018-06-03 10:13:13 -0700

    Commited my name

--- DevOperation2/Startup.cs
+++ DevOperation2/Startup.cs
@@ -27,7 +27,7 @@ namespace DevOperation2
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Noah.");
             });
         }
     }
